43049680bbb303ca71fa7c83201f9e34809f9575
RobertinoLirusso <robertinolirusso3@gmail.com>
2025-03-16 20:49:19 -0300

    new changes

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -8,57 +8,59 @@ import { ClubService } from '../../services/club.service';
 })
 export class CardComponent implements OnInit {
   
-    clubs: any[] = [];
-    selectedClub: any = null;
-    searchTerm: string = '';
-    
+  clubs: any[] = [];
+  selectedClub: any = null;
+  searchTerm: string = '';
+  visibleClubs: number = 100; // Mostrar inicialmente 100 clubes
   
-    constructor(private clubService: ClubService) {}
-  
-    ngOnInit(): void {
-      this.getClubs();
+  constructor(private clubService: ClubService) {}
+
+  ngOnInit(): void {
+    this.getClubs();
+  }
+
+  getClubs(): void {
+    this.clubService.getClubs().subscribe(data => {
+      this.clubs = this.shuffleArray(data);
+    });
+  }
+
+  shuffleArray(array: any[]): any[] {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]]; 
     }
-  
-    getClubs(): void {
-      this.clubService.getClubs().subscribe(data => {
-        this.clubs = data;
-        this.clubs = this.shuffleArray(data); 
-      });
+    return array;
+  }
+
+  openModal(index: number) {
+    this.selectedClub = this.filteredClubs[index];
+  }
+
+  closeModal() {
+    this.selectedClub = null;
+  }
+
+  get filteredClubs(): any[] {
+    if (!this.searchTerm) {
+      return this.clubs.slice(0, this.visibleClubs); // Solo mostrar hasta visibleClubs
     }
 
-    shuffleArray(array: any[]): any[] {
-      for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]]; // Intercambia elementos
-      }
-      return array;
-    }
-
-    openModal(index: number) {
-      this.selectedClub = this.filteredClubs[index];
-    }
+    const searchTermLower = this.searchTerm.toLowerCase();
   
-    closeModal() {
-      this.selectedClub = null;
-    }
+    return this.clubs.filter(club => 
+      club.club_name.toLowerCase().includes(searchTermLower) ||
+      club.city_country.toLowerCase().includes(searchTermLower)
+    ).slice(0, this.visibleClubs); // Aplicar límite después de filtrar
+  }
 
-    get filteredClubs(): any[] {
-      if (!this.searchTerm) {
-        return this.clubs;
-      }
-    
-      const searchTermLower = this.searchTerm.toLowerCase();
-    
-      return this.clubs.filter(club => 
-        club.club_name.toLowerCase().includes(searchTermLower) ||
-        club.city_country.toLowerCase().includes(searchTermLower) // Filtra por ciudad y país
-      );
-    }
-    
+  clearSearch() {
+    this.searchTerm = '';
+  }
 
-    clearSearch()  {
-      this.searchTerm = '';
-    }
+  loadMore() {
+    this.visibleClubs += 100; // Cargar 100 clubes adicionales
+  }
     
     
 }
